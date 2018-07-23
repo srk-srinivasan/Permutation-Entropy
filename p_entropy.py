4ea9adba21bcad0f2996ae0da66215225203d504
srk-srinivasan <srk.srinivasan@gmail.com>
2018-07-23 19:28:59 -0400

    Add files via upload

---
+++ p_entropy.py
@@ -0,0 +1,103 @@
+''' This module has essential functions supporting
+fast and effective computation of permutation entropy and
+its different variations.'''
+import itertools
+import numpy as np
+import pandas as pd
+from scipy.spatial.distance import euclidean
+from scipy.stats import kurtosis
+
+def s_entropy(freq_list):
+    ''' This function computes the shannon entropy of a given frequency distribution.
+    USAGE: shannon_entropy(freq_list)
+    ARGS: freq_list = Numeric vector represnting the frequency distribution
+    OUTPUT: A numeric value representing shannon's entropy'''
+    freq_list = [element for element in freq_list if element != 0]
+    sh_entropy = 0.0
+    for freq in freq_list:
+        sh_entropy += freq * np.log(freq)
+    sh_entropy = -sh_entropy
+    return(sh_entropy)
+
+def ordinal_patterns(ts, embdim, embdelay):
+    ''' This function computes the ordinal patterns of a time series for a given embedding dimension and embedding delay.
+    USAGE: ordinal_patterns(ts, embdim, embdelay)
+    ARGS: ts = Numeric vector represnting the time series, embdim = embedding dimension (3<=embdim<=7 prefered range), embdelay =  embdding delay
+    OUPTUT: A numeric vector representing frequencies of ordinal patterns'''
+    time_series = ts
+    possible_permutations = list(itertools.permutations(range(embdim)))
+    lst = list()
+    for i in range(len(time_series) - embdelay * (embdim - 1)):
+        sorted_index_array = list(np.argsort(time_series[i:(embdim+i)]))
+        lst.append(sorted_index_array)
+    lst = np.array(lst)
+    element, freq = np.unique(lst, return_counts = True, axis = 0)
+    freq = list(freq)
+    if len(freq) != len(possible_permutations):
+        for i in range(len(possible_permutations)-len(freq)):
+            freq.append(0)
+        return(freq)
+    else:
+        return(freq)
+
+def p_entropy(op):
+    ordinal_pat = op
+    max_entropy = np.log(len(ordinal_pat))
+    p = np.divide(np.array(ordinal_pat), float(sum(ordinal_pat)))
+    return(s_entropy(p)/max_entropy)
+
+def complexity(op):
+    ''' This function computes the complexity of a time series defined as: Comp_JS = Q_o * JSdivergence * pe
+    Q_o = Normalizing constant
+    JSdivergence = Jensen-Shannon divergence
+    pe = permutation entopry
+    ARGS: ordinal pattern'''
+    pe = p_entropy(op)
+    constant1 = (0.5+((1 - 0.5)/len(op)))* np.log(0.5+((1 - 0.5)/len(op)))
+    constant2 = ((1 - 0.5)/len(op))*np.log((1 - 0.5)/len(op))*(len(op) - 1)
+    constant3 = 0.5*np.log(len(op))
+    Q_o = -1/(constant1+constant2+constant3)
+
+    temp_op_prob = np.divide(op, sum(op))
+    temp_op_prob2 = (0.5*temp_op_prob)+(0.5*(1/len(op)))
+    JSdivergence = (s_entropy(temp_op_prob2) - 0.5 * s_entropy(temp_op_prob) - 0.5 * np.log(len(op)))
+    Comp_JS = Q_o * JSdivergence * pe
+    return(Comp_JS)
+
+def weighted_ordinal_patterns(ts, embdim, embdelay):
+    time_series = ts
+    possible_permutations = list(itertools.permutations(range(embdim)))
+    temp_list = list()
+    wop = list()
+    for i in range(len(time_series) - embdelay * (embdim - 1)):
+        Xi = time_series[i:(embdim+i)]
+        Xn = time_series[(i+embdim-1): (i+embdim+embdim-1)]
+        Xi_mean = np.mean(Xi)
+        Xi_var = (Xi-Xi_mean)**2
+        weight = np.mean(Xi_var)
+        sorted_index_array = list(np.argsort(Xi))
+        temp_list.append([''.join(map(str, sorted_index_array)), weight])
+    result = pd.DataFrame(temp_list,columns=['pattern','weights'])
+    freqlst = dict(result['pattern'].value_counts())
+    for pat in (result['pattern'].unique()):
+        wop.append(np.sum(result.loc[result['pattern']==pat,'weights'].values))
+    return(wop)
+
+def kweighted(ts, embdim, embdelay):
+    time_series = ts
+    possible_permutations = list(itertools.permutations(range(embdim)))
+    temp_list = list()
+    wop = list()
+    for i in range(len(time_series) - embdelay * (embdim - 1)):
+        Xi = time_series[i:(embdim+i)]
+        Xn = time_series[(i+embdim-1): (i+embdim+embdim-1)]
+        Xi_mean = np.mean(Xi)
+        Xi_var = (Xi-Xi_mean)**2
+        weight = kurtosis(Xi)
+        sorted_index_array = list(np.argsort(Xi))
+        temp_list.append([''.join(map(str, sorted_index_array)), weight])
+    result = pd.DataFrame(temp_list,columns=['pattern','weights'])
+    freqlst = dict(result['pattern'].value_counts())
+    for pat in (result['pattern'].unique()):
+        wop.append(np.sum(result.loc[result['pattern']==pat,'weights'].values))
+    return(wop)
